38ae2b521ffe74e35a767a48e05d38449454d809
ShinHeeSun <sbs2110@nate.com>
2022-04-05 16:14:08 +0900

    fist file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import logo from './img/logo.png';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} let="logo" alt="..." />
+      <h1>let`s go</h1>
+    </div>
+  );
+}
+export default App;
